52844826360efb346d74abf7a3f1388bca37c20b
Christopher Long <36999273+KingFruit85@users.noreply.github.com>
2022-12-19 21:56:30 +0000

    init commit

---
+++ deploy-commands.js
@@ -0,0 +1,52 @@
+const { REST, Routes } = require("discord.js");
+const dotenv = require("dotenv");
+const fs = require("node:fs");
+
+dotenv.config();
+
+const commands = [];
+// Grab all the command files from the commands directory you created earlier
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
+
+// and deploy your commands!
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+
+    // The put method is used to fully refresh all commands in the guild with the current set
+    // When finished in dev change to :
+
+    //  await rest.put(
+    //	Routes.applicationCommands(clientId),
+    //  { body: commands },
+    //  );
+
+    const data = await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`
+    );
+  } catch (error) {
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
+  }
+})();
