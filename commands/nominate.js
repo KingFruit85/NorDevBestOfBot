c5526913b23f5fd7b6840be2c0c4449bc6f8fd37
Christopher Long <36999273+KingFruit85@users.noreply.github.com>
2022-12-21 21:10:02 +0000

    FINE WILL

--- commands/nominate.js
+++ commands/nominate.js
@@ -1,12 +1,4 @@
-const {
-  SlashCommandBuilder,
-  Client,
-  GatewayIntentBits,
-} = require("discord.js");
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const dotenv = require("dotenv");
-dotenv.config();
-client.token = process.env.DISCORD_TOKEN;
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -17,28 +9,36 @@ module.exports = {
         .setName("input")
         .setDescription("the input to echo back")
         .setRequired(true)
-    )
-    .addChannelOption((option) =>
-      option.setName("channel").setDescription("The channel to echo into")
     ),
 
   async execute(interaction) {
-    // Get the username of the requestor
-    let user = interaction.user.username;
-    let input = interaction.options.getString("input");
+    let requestingUser = interaction.member.user;
+    let input = interaction.options.getString("input"); // Input is a message link
+
+    // ADD VALIDATION, return if invalid message link is provided
+
     let messageId = input.split("/")[input.split("/").length - 1];
     let channelId = input.split("/")[input.split("/").length - 2];
     let serverId = input.split("/")[input.split("/").length - 3];
 
+    // Get the channel object using the channel ID
+    const channel = interaction.client.channels.cache.get(channelId);
+
+    // Retrieve the message using the message ID
+    let message = await channel.messages.fetch(messageId);
+
+    const exampleEmbed = new EmbedBuilder()
+      .setDescription(message.content)
+      .setAuthor({
+        name: message.author.username,
+        iconURL: message.author.displayAvatarURL(),
+      });
+    exampleEmbed.setColor(0x7289da);
+
+    channel.send({ embeds: [exampleEmbed] }).catch(console.error);
+
     await interaction.reply(
-      `${user} has nominated the following message to be added to the best of list ${input}`
+      `${requestingUser} has nominated the following message to be added to the best of list`
     );
   },
 };
-
-// Or the nominate just checks the message link for an existing number of emojis?
-
-// Message link : https://discord.com/channels/1054500338947858522/1054500340063555606/1054524934459768862
-// [0] = Server Id
-// [1] = Channel Id
-// [2] = Message Id
