2f2fa583d345d6fa49dfdb4dbaeac554e1f19035
Christopher Long <36999273+KingFruit85@users.noreply.github.com>
2023-09-26 11:34:50 +0100

    wip

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,19 +1,22 @@
-export const name = Events.InteractionCreate;
-export async function execute(interaction) {
-  if (!interaction.isChatInputCommand()) return;
+module.exports = {
+  name: Events.InteractionCreate,
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand()) return;
 
-  const command = interaction.client.commands.get(interaction.commandName);
+    const command = interaction.client.commands.get(interaction.commandName);
 
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
+    if (!command) {
+      console.error(
+        `No command matching ${interaction.commandName} was found.`
+      );
+      return;
+    }
 
-  try {
-    console.log(`Executing ${interaction.commandName}`);
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(`Error executing ${interaction.commandName}`);
-    console.error(error);
-  }
-}
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(`Error executing ${interaction.commandName}`);
+      console.error(error);
+    }
+  },
+};
